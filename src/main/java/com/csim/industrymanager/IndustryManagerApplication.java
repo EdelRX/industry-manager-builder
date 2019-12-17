38a103d96bc45a994e3767161d7abcbe4683ab68
Edel Perez Cuellar <edel.perez@vass.es>
2019-12-17 11:44:33 +0100

    first commit

---
+++ src/main/java/com/csim/industrymanager/IndustryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.csim.industrymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustryManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csim/industrymanager/IndustryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csim.industrymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
